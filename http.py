95848bbb6ec289be03d49769c4a3c4f4ebdc8839
filip <fila.konemet@gmail.com>
2018-04-18 15:27:47 +0200

    add comments

--- http.py
+++ http.py
@@ -11,41 +11,110 @@ window = 5
 
 class ObjectData:
     def __init__(self, start=None, header=None):
+        """
+        Constructor setting up start of data object
+        :param start: start of object
+        :param header: header of object in some cases
+        """
         self.__start = start
         self.__length = 0
         self.__header = header
         self.__request = None
 
     def set_request(self, request):
+        """
+        setup request (header)
+        :param request:
+        :return: setter have no return
+        """
         self.__request = request
 
     def get_request(self):
+        """
+        getter of request
+        :return: request value
+        """
         return self.__request
 
     def set_length(self, value):
+        """
+        setup length
+        :param value: length value
+        :return:
+        """
         self.__length = value
 
     def add(self, value):
+        """
+        add value to length
+        :param value: value to add
+        :return:
+        """
         self.__length += value
 
     def length(self):
+        """
+
+        :return: length converted to int
+        """
         return int(self.__length)
 
     def start(self):
+        """
+
+        :return: begin of object like int
+        """
         return int(self.__start)
 
     def add_header(self, header):
+        """
+        setup header
+        :param header:
+        :return:
+        """
         self.__header = header
 
     def get_header(self):
+        """
+
+        :return: header value
+        """
         return self.__header
 
     def __str__(self):
+        """
+        get info about self
+        :return:  string start and length
+        """
         return "Start: "+str(self.__start)+"\nLength: "+str(self.__length)
+
+
+def check_duplicates(array):
+    """
+    check if key already exists in dict, if key exist, key will be change
+    :param array: array with duplicates
+    :return: dict with duplicates with changed duplicate-keys
+    """
+    count = 0
+    new_dict = {}
+    for k, v in array:
+        if k in new_dict:
+            new_dict[k+'_'+str(count)] = v
+            count = count + 1
+        else:
+            new_dict[k] = v
+    return new_dict
 
 
 class Protocol:
     def __init__(self, command_key, command, find_attr, pcap):
+        """
+
+        :param command_key: value for comparing
+        :param command: name of json field whose value is compared with command_key
+        :param find_attr:
+        :param pcap: source file
+        """
         self.__command = command.upper()
         self.__find_attr = find_attr
         self.__command_key = command_key
@@ -55,77 +124,175 @@ class Protocol:
         self.__client_port = None
         self.__error = None
         self.__error_code = None
-        self.__json_data = json.loads(self.__create_json(pcap))
+        self.__json_data = json.loads(self.__create_json(pcap), object_pairs_hook=check_duplicates)
         self.__obj_stack = []
         self.__request_value = None
 
     def __str__(self):
+        """
+        print information about all objects in obj_stack
+        :return:
+        """
         for obj in self.__obj_stack:
             print (obj)
         return ""
 
     def get_command(self):
+        """
+
+        :return: value of attribute command
+        """
         return self.__command
 
     def remove_request(self):
+        """
+        clear attribute request value and setup value None
+        :return:
+        """
         self.__request_value = None
 
     @staticmethod
     def __remove_duplicates(path):
+        """
+        run editcap for removing identical packets in file path
+        :param path: path to source file
+        :return:
+        """
         subprocess.Popen(["editcap", path, path, "-D", str(window)])
 
     @staticmethod
     def __create_json(pcap):
+        """
+        run tshark and return its result
+        :param pcap: source file
+        :return: json data from tshark
+        """
         Protocol.__remove_duplicates(pcap)
         p = subprocess.Popen(["tshark", "-r", pcap, "-T", "json", "-Y", "not tcp.analysis.retransmission and not tcp.analysis.fast_retransmission"], stdout=subprocess.PIPE)
         json_data, err = p.communicate()
         return json_data
 
     def server_ip(self):
+        """
+
+        :return: source ip
+        """
         return self.__server
 
     def client_ip(self):
+        """
+
+        :return: destination ip
+        """
         return self.__client
 
     def client_port(self):
+        """
+
+        :return: destination port
+        """
         return self.__client_port
 
     def set_error(self, error, error_code):
+        """
+        set up error values mostly from configuration file
+        :param error: JSON key
+        :param error_code: JSON value
+        :return:
+        """
         self.__error = error
         self.__error_code = error_code
 
     def remove_error(self):
+        """
+        used to for remove error options and reset obj_stack
+        attribute for new searching without errors
+        :return:
+        """
         self.__error = None
         self.__error_code = None
         self.clean_obj_stack()
 
     def clean_obj_stack(self):
+        """
+        erase all object from stack
+        :return:
+        """
         del self.__obj_stack[:]
 
     def server_port(self):
+        """
+
+        :return: source port
+        """
         return self.__server_port
 
     def obj_count(self):
-        print (self.__obj_stack)
+        """
+
+        :return: count of items in obj_stack attribute
+        """
         return len(self.__obj_stack)
 
     def __remove_last(self):
+        """
+        remove last searched object
+        :return:
+        """
         self.__obj_stack = self.__obj_stack[:-1]
 
     def __add_object(self, obj):
+        """
+        add new object at top of stack
+        :param obj: reference to new object
+        :return:
+        """
         self.__obj_stack.append(obj)
 
     def get_object(self, index):
+        """
+        return object from obj_stack on index position
+        :param index: index into "stack" actually from array
+        :return: reference to object
+        """
         return self.__obj_stack[index]
 
     def get_obj_list_len(self):
+        """
+
+        :return: count of objects in obj_stack
+        """
         return len(self.__obj_stack)
 
     def get_last_object(self):
+        """
+
+        :return: return last added object from obj_stack attribute
+        """
         return self.__obj_stack[len(self.__obj_stack)-1:][0]
 
+    def find_field(self, key):
+        """
+        search key in JSON and return value form key field
+        :param key: searched json field from json_data
+        :return: return found value or None
+        """
+        value = None
+        for packet in self.__json_data:
+            value = self.__search(key, packet)
+            if value is not None:
+                self.__server = self.__search("ip.src", packet)
+                self.__client = self.__search("ip.dst", packet)
+                self.__client_port = self.__search("tcp.dstport", packet)
+                self.__server_port = self.__search("tcp.srcport", packet)
+                return value
+        return None
+
     def __search(self, pattern, d):
-        """search key in nested dict, if found return value of key"""
+        """
+        search key in nested dict, if found return value of key
+        :return: found string value
+        """
         if pattern in d:
             return d[pattern]
         else:
@@ -137,17 +304,25 @@ class Protocol:
 
     @staticmethod
     def __read_json(filename):
+        """
+        open file and load json data
+        :param filename: path to json file
+        :return: json object
+        """
         with open(filename) as my_file:
             return json.load(my_file)
 
-    def find_length(self, protocol_rules = None):
-        """ find starts and lengths of all sending object and store it into list of ObjectData"""
+    def find_length(self, protocol_rules=None):
+        """
+        find starts and lengths of all sending object and store it into list of ObjectData
+        setup ports, ips
+        :param protocol_rules rules from config file
+        """
         command_found = False   # flag True if object was found
         header_flag = False
         header = None
 
         tmp_header_value = None
-
         for packet in self.__json_data: # loop over each packet
             tmp_header_value = None
             if protocol_rules is not None and protocol_rules['request'] != "":
@@ -203,6 +378,11 @@ class Protocol:
 
 class HexaData:
     def __init__(self, file_path):
+        """
+        constructor, sets up ip regex
+        runs tcpflow
+        :param file_path: path to source file
+        """
         self.__ip_regex = r"^\d{3}\.\d{3}.\d{3}\.\d{3}.\d{5}-\d{3}\.\d{3}.\d{3}\.\d{3}.\d{5}\:"
         self.__ip = {}
 
@@ -217,8 +397,9 @@ class HexaData:
         self.__split_data()
 
     def __split_data(self):
-        """split hexa data based on end of line , each line append into
-            right field in ip list
+        """
+        split hexa data based on end of line , each line append into right field in ip list
+
         """
         actual_ip = None
         for line in self.__tcpflow_data.split('\n'):
@@ -241,7 +422,11 @@ class HexaData:
 
     @staticmethod
     def convert_ip(ip):
-        """add 0 into ip address 10.10.10.10 -> 010.010.010.010"""
+        """
+        adds 0 into ip address 10.10.10.10 -> 010.010.010.010
+
+        :return converted ip
+        """
         octets = ip.split('.')
         final_ip = ""
         i = 0
@@ -261,7 +446,27 @@ class HexaData:
 
         return final_ip[:-1]
 
+    def write_value(self, filename, value):
+        """
+        write data from value to file with name filename
+
+        :param filename: name of created file
+        :param value: data to write
+        :return:
+        """
+        with open(filename, 'wb') as out:
+            out.write(value)
+
     def write_data(self, source_name, dest_name, protocol, delimiter=None):
+        """
+        read specific part of data from source_name file depending on protocol and delimiter and write it into dest_name
+        file
+        :param source_name: name of input file
+        :param dest_name: name file which wil be creat
+        :param protocol: object with informations about user objects
+        :param delimiter: if is set all data before delimiter are cutted
+        :return:
+        """
         with open("hexa/" + source_name + ".data", 'rb') as myfile:
 
             i = 0
@@ -299,6 +504,10 @@ class HexaData:
                 i = i+1
 
     def __del__(self):
+        """
+        destructor use to for remove temporary directory
+        :return:
+        """
         os.system('rm -r hexa/')
 
 
@@ -374,8 +583,7 @@ class Initialization:
                 file.close(out)
             if os.path.exists("report.xml"):
                 subprocess.call(["rm", "report.xml"])
-            print "Creating file: "+ name # TODO pridat jmeno
-
+            print "Creating file: "+ name
     def test_object_count(self):
 
         if self.__protocol.get_obj_list_len() == 0:
@@ -405,17 +613,30 @@ class Initialization:
                                     self.__protocol)
         self.__protocol.clean_obj_stack()
 
+    def level4(self):
+        data = self.__protocol.find_field(self.__protocol_rules['json_field'])
+        name = self.__protocol.server_ip()+":"+self.__protocol.server_port()+"-" + self.__protocol.client_ip()+":"+self.__protocol.client_port()
+        self.__hexa_data.write_value(name, data)
+
+        print "Creating file: " + name
+
+
     def run(self):
-        if "level" not in self.__args or self.__args.level == "1":
-            self.level1()
-        elif "level" in self.__args and self.__args.level == "2":
-            self.level2()
-        elif "level" in self.__args and self.__args.level == "3":
-            self.level3()
+        r = self.__protocol_rules
+        if (r['json_field']!= "" and r['command'] == "" and r['request'] == "" and r['request_parameter'] == ""
+            and r['error'] == "" and r['error_value'] == ""):
+            self.level4()
         else:
-            self.level1()
-            self.level2()
-            self.level3()
+            if "level" not in self.__args or self.__args.level == "1":
+                self.level1()
+            elif "level" in self.__args and self.__args.level == "2":
+                self.level2()
+            elif "level" in self.__args and self.__args.level == "3":
+                self.level3()
+            else:
+                self.level1()
+                self.level2()
+                self.level3()
 
 
 init = Initialization()
